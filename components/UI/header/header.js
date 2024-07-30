b85988829092b9622524fb19cab18640bab56edf
AkamalKazi <kaziakmal2526@gmail.com>
2024-07-30 09:21:06 +0530

    Update header.js

--- components/UI/header/header.js
+++ components/UI/header/header.js
@@ -11,7 +11,7 @@ const HeaderComponent = () => {
               className="flex justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
               id="mobile-menu-2"
             >
-              <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+              <ul className="flex  mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                 <li>
                   <Link
                     href="/dog"
